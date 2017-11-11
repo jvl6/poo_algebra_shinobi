26d6f9b3c0885e1b1ce05eadca4d9c5c2892af28
jvlee6 <jvergaralee@gmail.com>
2017-11-11 14:03:02 -0300

    Revirtiendo al commit anterior.

--- proyecto_algebra/src/main/Main.java
+++ proyecto_algebra/src/main/Main.java
@@ -1,24 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package main;
-
-import java.util.Scanner;
 
 /**
  *
- * Javier Vergara / Sergio Herrera / Juan Catrileo
+ * @author ZDK
  */
 public class Main {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-               Scanner scan = new Scanner (System.in);
-       double n1 ,n2 , resultado , resultado2;
-        System.out.println("ingrese el numero 1:");
-        n1=scan.nextDouble();
-        System.out.println("ingrese el numero 2:");
-        n2=scan.nextDouble();
-        resultado = Math.sqrt(n1);
-        resultado2 = Math.sqrt(n2);
-        System.out.println("la raiz de n1 y  es:" + resultado + "el resultado de n2 es" +resultado2);
-
+        // TODO code application logic here
     }
     
 }
